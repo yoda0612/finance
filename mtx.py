4da14e0650519a4175264a2bfa22f452217312ac
Chinyi Cheng <yoda670612@gmail.com>
2023-06-19 16:12:58 +0900

    6/19

--- mtx.py
+++ mtx.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import os
-
+import threading
+result_all=np.empty((120,116),dtype=object)
 # for m in range(1,121):
 #     with open("Min"+str(m)+".csv","r") as fr:
 #         with open("Min" + str(m) + ".tmp", "w") as fw:
@@ -21,49 +22,59 @@ def get_result(file_number, ma_period):
     df["CrossDown"] = (df["Close"] < df["MA"]) & (df["Close"] > df["MA"]).shift(1)
 
     rateList = []
-
-    file_index=1
+    # file_index=1
     for i in range(len(df)):
         if df.loc[i]["CrossUp"]:
             for j in range(i, len(df)):
                 if df.loc[j]["CrossDown"]:
-                    max = df.loc[i + 1:j, "Close"].max()
+                    max = df.loc[i + 1:j, "High"].max()
                     start = df.loc[i, "Close"]
                     rate = max / start
                     rateList.append(rate)
 
-                    print(df.loc[i:j, ["Date","Close","MA"]])
-                    df.loc[i:j, ["Date", "Close", "MA"]].to_csv("result_"+str(file_index)+".csv")
-                    file_index+=1
+                    #print(df.loc[i:j, ["Date","Close","MA"]])
+                    # df.loc[i:j, ["Date", "Close", "MA"]].to_csv("result_"+str(file_index)+".csv")
+                    # file_index+=1
 
                     break
 
-    return np.array(rateList).mean(), np.array(rateList).max(), np.array(rateList).min()
+    result_all[file_number-1,ma_period-5] = (np.array(rateList).mean(), np.array(rateList).max(), np.array(rateList).min())
+    print(file_number, ma_period)
 
-get_result(64,46)
 
-# result_all=[]
-#
-# for m in range(1,121):
-#     result_row = []
-#
-#     for ma_period in range(5, 121):
-#         print(m, ma_period)
-#         result_row.append(get_result(m, ma_period))
-#
-#     result_all.append(result_row)
-#
-# with open("mean.csv","w") as f:
-#     with open("max.csv", "w") as f_max:
-#         with open("min.csv", "w") as f_min:
-#             for row in result_all:
-#                 for mean, max, min in row:
-#                     f.write(str(mean) + ",")
-#                     f_max.write(str(max) + ",")
-#                     f_min.write(str(min) + ",")
-#                 f.write("\n")
-#                 f_max.write("\n")
-#                 f_min.write("\n")
+
+
+t=[]
+for m in range(1,121): #121
+
+    for ma_period in range(5, 121): #121
+        #print(m, ma_period)
+        t.append(threading.Thread(target=get_result, args=(m, ma_period,)))
+        #result_row.append(get_result(m, ma_period))
+
+    #result_all.append(result_row)
+
+
+for tt in t:
+    tt.start()
+for tt in t:
+   tt.join()
+print(result_all)
+
+
+with open("mean.csv","w") as f:
+    with open("max.csv", "w") as f_max:
+        with open("min.csv", "w") as f_min:
+            for rowIndex in range(result_all.shape[0]):
+                for colIndex in range(result_all.shape[1]):
+                    mean, max, min = result_all[rowIndex,colIndex]
+
+                    f.write(str(mean) + ",")
+                    f_max.write(str(max) + ",")
+                    f_min.write(str(min) + ",")
+                f.write("\n")
+                f_max.write("\n")
+                f_min.write("\n")
 
 
 
